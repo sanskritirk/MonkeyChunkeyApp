2af38e252d7d4bb13d7d34fcc10c543b9057b30e
sanskritirk <kanagalasanskriti@gmail.com>
2020-08-19 17:23:02 +0530

    'c63'

--- App.js
+++ App.js
@@ -1,15 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,TextInput,TouchableOpacity} from 'react-native';
+import {Header} from 'react-native-elements';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component{
+ constructor(){
+   super()
+   this.state={
+     text:' ',
+     displayText:' '
+   }
+ }
+ render(){
+   return(
+     <View>
+     <Header
+     backgroundColor={'orange'}
+     centerComponent={{text:'Monkey Chunkey',style:{color:'black',fontSize:20}}}/>
+     <TextInput
+     onChangeText={(text)=>{
+       this.setState({
+         text:text
+       })
+     }}
+     value={this.state.text}
+     style={styles.inputBox}/>
+     <TouchableOpacity style={styles.button} onPress={()=>{
+       this.setState({
+         displayText:this.state.text
+       })
+     }}>
+     <Text style={styles.btntxt}>Go</Text>
+     </TouchableOpacity>
+     <Text style={styles.displaytxt}>{this.state.displayText}</Text>
+     </View>
+   );
+ } 
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -18,4 +45,37 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  inputBox:{
+    marginTop:200,
+    width:'80%',
+    alignSelf:'center',
+    borderWidth:4,
+    height:40,
+    textAlign:'center'
+  },
+  button:{
+    backgroundColor:'red',
+    width:'50%',
+    height:45,
+    alignSelf:'center',
+    marginTop:20,
+    borderRadius:10
+  },
+  btntxt:{
+    textAlign:'center',
+    color:'white',
+    fontSize:25,
+    fontWeight:'bold',
+    justifyContent:'center',
+    marginTop:4
+  },
+  displaytxt:{
+    margin:15,
+    fontSize:25,
+    textAlign:'center',
+    justifyContent:'center',
+    fontWeight:'bold',
+    marginTop:20,
+    padding:20
+  }
 });
