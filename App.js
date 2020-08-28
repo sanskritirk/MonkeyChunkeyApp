4cd1b645edd6dca4a837ea8ae5f4d3de47c9d37f
sanskritirk <kanagalasanskriti@gmail.com>
2020-08-28 17:34:28 +0530

    'FinalApp'

--- App.js
+++ App.js
@@ -1,81 +1,108 @@
-import React from 'react';
-import { StyleSheet, Text, View,TextInput,TouchableOpacity} from 'react-native';
-import {Header} from 'react-native-elements';
+import * as React from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  Alert
+} from 'react-native';
+import { Header } from 'react-native-elements';
+import db from './localDb';
+import PhonicSoundButton from './components/PhonicSoundButton';
 
-export default class App extends React.Component{
- constructor(){
-   super()
-   this.state={
-     text:' ',
-     displayText:' '
-   }
- }
- render(){
-   return(
-     <View>
-     <Header
-     backgroundColor={'orange'}
-     centerComponent={{text:'Monkey Chunkey',style:{color:'black',fontSize:20}}}/>
-     <TextInput
-     onChangeText={(text)=>{
-       this.setState({
-         text:text
-       })
-     }}
-     value={this.state.text}
-     style={styles.inputBox}/>
-     <TouchableOpacity style={styles.button} onPress={()=>{
-       this.setState({
-         displayText:this.state.text
-       })
-     }}>
-     <Text style={styles.btntxt}>Go</Text>
-     </TouchableOpacity>
-     <Text style={styles.displaytxt}>{this.state.displayText}</Text>
-     </View>
-   );
- } 
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      text: '',
+      chunks: [],
+      phonicSounds: [],
+    };
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Header
+          backgroundColor={'#9c8210'}
+          centerComponent={{
+            text: 'Monkey Chunky',
+            style: { color: '#fff', fontSize: 20 },
+          }}
+        />
+
+        <Image
+          style={styles.imageIcon}
+          source={{
+            uri:
+              'https://www.shareicon.net/data/128x128/2015/08/06/80805_face_512x512.png',
+          }}
+        />
+
+        <TextInput
+          style={styles.inputBox}
+          onChangeText={text => {
+            this.setState({ text: text });
+          }}
+          value={this.state.text}
+        />
+        <TouchableOpacity
+          style={styles.goButton}
+          onPress={() => {
+            var word=this.state.text.toLowerCase().trim()
+            db[word]?(
+            this.setState({ chunks: db[word].chunks }),
+            this.setState({ phonicSounds: db[word].phones })
+            ):
+            Alert.alert("Word does not exist in the database")
+          }}>
+          <Text style={styles.buttonText}>GO</Text>
+        </TouchableOpacity>
+        <View>
+          {this.state.chunks.map((item, index) => {
+            return (
+              <PhonicSoundButton
+                wordChunk={this.state.chunks[index]}
+                soundChunk={this.state.phonicSounds[index]}
+                buttonIndex={index}
+              />
+            );
+          })}
+        </View>
+      </View>
+    );
+  }
 }
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#b8b8b8',
   },
-  inputBox:{
-    marginTop:200,
-    width:'80%',
-    alignSelf:'center',
-    borderWidth:4,
-    height:40,
-    textAlign:'center'
+  inputBox: {
+    marginTop: 50,
+    width: '80%',
+    alignSelf: 'center',
+    height: 40,
+    textAlign: 'center',
+    borderWidth: 4,
   },
-  button:{
-    backgroundColor:'red',
-    width:'50%',
-    height:45,
-    alignSelf:'center',
-    marginTop:20,
-    borderRadius:10
+  goButton: {
+    width: '50%',
+    height: 55,
+    alignSelf: 'center',
+    padding: 10,
+    margin: 10,
   },
-  btntxt:{
-    textAlign:'center',
-    color:'white',
-    fontSize:25,
-    fontWeight:'bold',
-    justifyContent:'center',
-    marginTop:4
+  buttonText: {
+    textAlign: 'center',
+    fontSize: 30,
+    fontWeight: 'bold',
   },
-  displaytxt:{
-    margin:15,
-    fontSize:25,
-    textAlign:'center',
-    justifyContent:'center',
-    fontWeight:'bold',
-    marginTop:20,
-    padding:20
+  imageIcon: {
+    width: 150,
+    height: 150,
+    marginLeft: 95,
   }
 });
---
+++ components/PhonicSoundButton.js
@@ -0,0 +1,64 @@
+import * as React from 'react';
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
+import { Audio } from 'expo-av';
+
+export default class PhonicSoundButton extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      pressedButtonIndex:''
+    }
+  }
+  playSound = async soundChunk => {
+    console.log(soundChunk);
+    var soundLink =
+      'https://whitehatjrcontent.s3.ap-south-1.amazonaws.com/phones/' +
+      soundChunk +
+      '.mp3';
+    await Audio.Sound.createAsync(
+      {
+        uri: soundLink,
+      },
+      { shouldPlay: true }
+    );
+  };
+  render() {
+    return (
+      <TouchableOpacity
+        style={
+          this.props.index === this.state.pressedButtonIndex
+          ?[styles.chunkButton,{backgroundColor:'white'}]
+          :[styles.chunkButton,{backgroundColor:'red'}]
+        }
+        onPress={() => {
+          this.playSound(this.props.soundChunk);
+          this.setState({
+            pressedButtonIndex:this.props.index
+          })
+        }}>
+        <Text style={
+          this.props.index === this.state.pressedButtonIndex
+          ?[styles.displayText,{color:'black'}]:[styles.displayText,{color:'white'}]
+        }>
+        {this.props.wordChunk}</Text>
+      </TouchableOpacity>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  displayText: {
+    textAlign: 'center',
+    fontSize: 30,
+    color: 'white'
+  },
+  chunkButton:{
+    width: '60%',
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    borderRadius: 10,
+    margin: 5,
+    backgroundColor: 'red'
+  }
+});
